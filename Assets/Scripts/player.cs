60992b1b9f23f10e96495c1a58a7b35372747df0
Darren <dmeidl15@gmail.com>
2023-09-28 10:32:06 +0100

    Moved player script to folder, wrote code in start method

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    //1 reference
+    public float velocity = 2.4f;
+    //2 references
+    private float 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
