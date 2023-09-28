463cdf1c635b9d5e2988d074e8a88c4e3acf8ccd
Darren <dmeidl15@gmail.com>
2023-09-28 11:54:41 +0100

    Added code to obstacle & spawners scripts

--- Assets/obstacle.cs
+++ Assets/obstacle.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class obstacle : MonoBehaviour
 {
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class obstacle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += ((Vector3.left* speed)*Time.deltaTime);
     }
 }
--- Assets/spawners.cs
+++ Assets/spawners.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class spawners : MonoBehaviour
 {
+    public float queueTime = 1.5f;
+    private float time = 0;
+    public GameObject object;
+    public float height;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,13 @@ public class spawners : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(time>queueTime){
+            GameObject go = Instantiate(obstacle);
+            go.tranform.position = transform.position + 
+            new Vector3(0,Random.Range(-height,height), 0);
+            time = 0 ;
+            Destroy(go, 10);
+        }
+        time += Time.deltaTime;
     }
 }
