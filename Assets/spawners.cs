ff202c67df538b8ae2a695b28d28ba4f85d4420e
Darren <dmeidl15@gmail.com>
2023-09-28 13:01:50 +0100

    Attempted to fix compiling errors in scripts

--- Assets/spawners.cs
+++ Assets/spawners.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawner : MonoBehaviour
+public class spawners : MonoBehaviour
 {
     public float queueTime = 1.5f;
     private float time = 0;
-    public GameObject object;
+    public GameObject obstacle;
     public float height;
     // Start is called before the first frame update
     void Start()
