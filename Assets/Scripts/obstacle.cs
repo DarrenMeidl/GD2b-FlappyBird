24b4bd9d4b434c479bc38c68fc91c6e311563a1f
Darren <dmeidl15@gmail.com>
2023-10-01 15:55:06 +0100

    Gave pipes speed of 1
    Issue where pipes won't continue to spawn

--- Assets/Scripts/obstacle.cs
+++ Assets/Scripts/obstacle.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class obstacle : MonoBehaviour
+public class Obstacle : MonoBehaviour
 {
     public float speed;
     // Start is called before the first frame update
@@ -14,6 +14,6 @@ public class obstacle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += ((Vector3.left* speed)*Time.deltaTime);
+        transform.position += ((Vector3.left * speed) * Time.deltaTime);
     }
 }
