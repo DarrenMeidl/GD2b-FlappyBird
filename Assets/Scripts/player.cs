38f7a6706c8bdb43f8e43f4ea7834f35ca269e65
Darren <dmeidl15@gmail.com>
2023-10-05 09:19:57 +0100

    Fixed not restarting issue

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -24,7 +24,7 @@ public class Player : MonoBehaviour
         }
     }
     private void OnCollisionEnter2D(Collision2D other){
-        if (other.gameObject.name == "Shield"){
+        if (other.gameObject.name == "Shield" && isDead == false){
             isDead = false;
         }
         else {
